dd24d9d631532ee9c8525fa90d918043d1c54f7c
Ed Allonby <edward.allonby@gmail.com>
2018-11-19 20:04:05 +0000

    Add enumerable extension tests and a todo comment

---
+++ Sequencer.Utilities.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Sequencer.Utilities.Tests
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        private static readonly object[] YieldCases =
+        {
+            new object[] {0, new List<int> {0}},
+            new object[] {"hello", new List<string> {"hello"}},
+            new object[] {new List<int> {1}, new List<List<int>> {new List<int> {1}}}
+        };
+
+        [Test]
+        [TestCaseSource(nameof(YieldCases))]
+        public void YieldTests(object thing, object expected)
+        {
+            IEnumerable<object> actual = thing.Yield();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Sequencer.Utilities/EnumerableExtensions.cs
+++ Sequencer.Utilities/EnumerableExtensions.cs
@@ -8,6 +8,12 @@ namespace Sequencer.Utilities
         /// <summary>
         /// Wraps this object instance into an <see cref="IEnumerable{T}" /> consisting of a single item.
         /// </summary>
+        /// <remarks>
+        /// TODO:
+        /// This is a pure part of an Applicative, or a return for a Monad.
+        /// What we could do here is create a new Monadic interface and try to figure out why we're using this yield,
+        /// Maybe it is just used later with a bind/join.
+        /// </remarks>
         /// <typeparam name="T"> Type of the object. </typeparam>
         /// <param name="item"> The instance that will be wrapped. </param>
         /// <returns> An <see cref="IEnumerable{T}" /> consisting of a single item. </returns>
